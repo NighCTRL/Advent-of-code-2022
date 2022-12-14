4235222847717ae2b6f232a1b6597e9b439374b2
nigh <code@nigh.one>
2022-12-14 22:33:25 +0100

    Day one

---
+++ src/bin/day_1.rs
@@ -0,0 +1,21 @@
+use anyhow::Result;
+
+fn main() -> Result<()> {
+    let mut max = include_str!("./day_1.input")
+        .split("\n\n")
+        .map(|x| {
+            x.lines()
+                .flat_map(str::parse::<usize>)
+                .sum::<usize>()
+        })
+        .collect::<Vec<usize>>();
+
+    max.sort_by(|a, b| b.cmp(a));
+    
+    println!("The top 3 elves carry {:?}", max
+        .iter()
+        .take(3)
+        .sum::<usize>());
+
+    return Ok(());
+}
