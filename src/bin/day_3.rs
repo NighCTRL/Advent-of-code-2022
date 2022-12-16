4664bf9b941ee9ab139c9eb2a0ac4fc09495b101
nigh <code@nigh.one>
2022-12-16 17:16:45 +0100

    Day 3 part 1

--- src/bin/day_3.rs
+++ src/bin/day_3.rs
@@ -1,3 +1,31 @@
-fn main () {
-    let string = include_str!("../inputs/day_3.input");
+const START_LOWER: u8 = b'a' - 1;
+const START_UPPER: u8 = b'A' - 1;
+use anyhow::Result;
+use std::collections::HashSet;
+
+fn main () -> Result<()> {
+    let points = std::fs::read_to_string("src/inputs/day_3.input")?
+        .lines()
+        .flat_map(|line| {
+            let (part1, part2) = line.split_at(line.len() / 2);
+            let part1 = part1.chars().collect::<HashSet<_>>();
+            return part2
+                .chars()
+                .filter(move |c| part1.contains(c))
+                .collect::<HashSet::<char>>()
+                .into_iter()
+                .map(|c| {
+                    let value = if c.is_ascii_lowercase() {
+                        c as u8 - START_LOWER
+                    } else {
+                        c as u8 - START_UPPER + 26
+                    };
+                    println!("C: {} value: {}", c, value);
+                    return value;
+                })
+                .map(|c| c as u32)
+        })
+        .sum::<u32>();
+    println!("Result: {}", points);
+    return Ok(());
 }
